bef2938aa28cfbca433fbee8ab3face069266383
Michael Kyulu <michaelkyulu4@gmail.com>
2025-07-13 12:12:04 +0300

    Adding a new python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10 #Initializing the integers
+number2 = 5  #Initializing the second integer
+
+sum = number1 + number2 #Defining the variable sum
+difference = number2 - number1 #subtraction and defining variable difference
+product = number1 * number2
+
+print("sum of" number1 "and" number2 "is" sum)
+print("Difference of" number2 "and" number1 "is" difference)
+print("product of" number1 "and" number2 "is" product)
