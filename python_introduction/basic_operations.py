024c883deaab4e0c42d910818dee3a1d35fd2228
Michael Kyulu <michaelkyulu4@gmail.com>
2025-07-13 12:29:13 +0300

    I debugged the code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,6 +5,6 @@ sum = number1 + number2 #Defining the variable sum
 difference = number2 - number1 #subtraction and defining variable difference
 product = number1 * number2
 
-print("sum of" number1 "and" number2 "is" sum)
-print("Difference of" number2 "and" number1 "is" difference)
-print("product of" number1 "and" number2 "is" product)
+print("sum of", number1, "and", number2, "is", sum)
+print("Difference of", number2, "and", number1, "is", difference)
+print("product of", number1, "and", number2, "is", product)
