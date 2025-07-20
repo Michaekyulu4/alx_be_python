357e472c790fd9a3eacaf73763ec52013d718ebe
Michael Kyulu <michaelkyulu4@gmail.com>
2025-07-20 21:36:29 +0300

    Adding this file  match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,27 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print("The result is",result,".")
+
+    case "-":
+        result = num1 - num2
+        print("The result is",result,".")
+
+    case "*":
+        result = num1 * num2
+        print("The result is",result,".")
+
+    case "/":
+        if num1 != 0:
+            result = num1 / num2
+            print("The result is",result,".")
+        else:
+            print("Error: Cannot devide by zero.")
+
+    case _:
+        print("Invalid operation")
+
