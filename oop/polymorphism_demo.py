a97460b74a2c822d7faf8374db05b37de1c4041d
Michael Kyulu <michaelkyulu4@gmail.com>
2025-08-17 20:20:10 +0300

    Area of a shape

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,18 @@
+class Shape:
+    def area(self):
+         raise NotImplementedError("The area() method must be overridden in a subclass")
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
